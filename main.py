0ee4d46332172ca9f1d8af25137ce38eb7466fe3
Carlos Alba <calba5141114@Carloss-MacBook-Pro.local>
2018-08-29 21:14:23 -0700

    done for the first time

---
+++ main.py
@@ -0,0 +1,10 @@
+adjective1 = raw_input("Tell me an adjective, and click enter. ")
+noun1 = raw_input("Tell me a noun, and click enter. ")
+noun2 = raw_input("Tell me another noun, and click enter. ")
+adjective2 = raw_input("Tell me an another adjective, and click enter. ")
+
+print "\n Madlibs \n"
+print "The banana is " + adjective1 + ' and was made by '
+print noun1 + " they are also eaten by"
+print noun2 + " and is super super super " + adjective2
+print "So you can thank " + noun1 + "for the banana"
